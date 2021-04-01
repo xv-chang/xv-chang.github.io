8c62da5ef0eb5dccdf25e60cabb00ee8bf522234
许昶 <xv_chang@qq.com>
2021-04-01 13:08:15 +0800

    Site updated: 2021-04-01 13:08:14

---
+++ js/js.js
@@ -0,0 +1,67 @@
+var web_style = $("#web_style").val();
+
+
+var gitalk = new Gitalk({
+    clientID: '70c44fb0d2dbd9d79c0d',
+    clientSecret: 'cfcf187449eb8d324420aa17fe3be461aa98b55e',
+    repo: 'xv-chang.github.io',
+    owner: 'xv-chang',
+    admin: ['xv-chang'],
+    id: location.pathname,      // Ensure uniqueness and length less than 50
+    distractionFreeMode: false  // Facebook-like distraction free mode
+})
+
+gitalk.render('gitalk-container')
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+});
+
+function setCookie(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getCookie(key) {
+    var data = localStorage.getItem(key);
+    return data
+}
+
+function updateStyle() {
+    if (getCookie("style") == "white") {
+        $("#footer").attr("style", "color: #51525d;");
+        $(".flink").attr("style", "color: #51525d;");
+        $(".ba").attr("style", "color: #51525d;");
+        $("#bodyx").attr("class", "bg_while");
+        $("#update_style").attr('checked', false);
+    } else {
+        $("#footer").attr("style", "");
+        $(".flink").attr("style", "");
+        $("#bodyx").attr("class", "");
+        $(".ba").attr("style", "");
+        $("#update_style").attr('checked', true);
+    }
+}
+
+if (getCookie("style") == null) {
+    setCookie("style", web_style)
+    updateStyle();
+} else if (getCookie("style") == "white") {
+    setCookie("style", "white")
+    updateStyle();
+} else if (getCookie("style") == "black") {
+    setCookie("style", "black")
+    updateStyle();
+}
+
+$("#update_style").change(function () {
+    var style = $("#update_style").is(':checked');
+    if (style) {
+        setCookie("style", "black")
+        updateStyle();
+    } else {
+        setCookie("style", "white")
+        updateStyle();
+    }
+});
